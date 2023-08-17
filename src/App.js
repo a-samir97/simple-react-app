1da7c4d4a98ba86e5f6c13974e4a02b15d2aa8b1
Ahmed Samir <ahmed.samir@thndr.app>
2023-08-18 01:04:23 +0300

    Use useState to remember data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import {useState} from 'react'
 import './App.css';
 
-function Square({value}) {
+function Square() {
+  const [value, setValue] = useState(null);
+
   function handleClick() {
-    console.log("Button Clicked")
+    setValue('X')
   }
   return <button className='square' onClick={handleClick}>{value}</button>
 }
@@ -11,21 +14,21 @@ function App() {
   return (
     <div className="App">
       <div className='board-row'>
-        <Square value={1}/>
-        <Square value={2}/>
-        <Square value={3}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div className='board-row'>
-        <Square value={4}/>
-        <Square value={5}/>
-        <Square value={6}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div className='board-row'>
-        <Square value={7}/>
-        <Square value={8}/>
-        <Square value={9}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
     </div>
