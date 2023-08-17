f5e9788ddb26b2b93d53dc588e9514f99b97bea7
Ahmed Samir <ahmed.samir@thndr.app>
2023-08-18 01:43:50 +0300

    pass parent states as a child props to call handleClick function for every square component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import {useState} from 'react'
 import './App.css';
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X')
-  }
-  return <button className='square' onClick={handleClick}>{value}</button>
+function Square({value, squareClick}) {
+  return <button className='square' onClick={squareClick}>{value}</button>
 }
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares)    
+  }
   return (
     <div className="App">
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} squareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} squareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} squareClick={() => handleClick(2)}/>
       </div>
 
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} squareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} squareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} squareClick={() => handleClick(5)}/>
       </div>
 
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} squareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} squareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} squareClick={() => handleClick(8)}/>
       </div>
 
     </div>
